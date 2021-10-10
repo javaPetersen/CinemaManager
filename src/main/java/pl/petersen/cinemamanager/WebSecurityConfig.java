a54085950627245f963271d5cf308c535fe57310
Piotr Perlinski <perlinskipiotr@wp.pl>
2021-10-10 14:14:54 +0200

    update readme and maven

--- src/main/java/pl/petersen/cinemamanager/WebSecurityConfig.java
+++ src/main/java/pl/petersen/cinemamanager/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package pl.petersen.cinemamanager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.petersen.cinemamanager.entity.User;
+import pl.petersen.cinemamanager.repository.UserRepository;
+import pl.petersen.cinemamanager.service.UserService;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +23,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -43,6 +55,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
-
 
 }
